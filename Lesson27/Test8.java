00381394d6d0cc0b143e9be06391f62a48d6f255
VegasJava <seninlsa22@gmail.com>
2022-12-23 15:02:14 +0400

    continue Lesson28 from 30:00

---
+++ Lesson27/Test8.java
@@ -0,0 +1,33 @@
+
+package Lesson27;
+import java.io.*;
+
+public class Test8 {
+
+    static StringBuilder abc() {
+        StringBuilder a = new StringBuilder ("privet");
+        try {
+            File f = new File("test10.txt");
+            FileInputStream fis = new FileInputStream(f);
+        
+        } catch (FileNotFoundException e) {
+            System.out.println("Bil poyman Exception");
+            System.out.println("peremennaia a v catch bloke = " + a);
+        return a;
+        } finally {
+            a.append("!!!");
+            System.out.println("eto block finaly");
+            System.out.println("peremennaia a v catch bloke = " + a);
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(abc());
+
+
+        
+        
+    }
+}
---
+++ Lesson27/Test9.java
@@ -0,0 +1,27 @@
+package Lesson27;
+
+import java.io.*;
+
+public class Test9 {
+
+    static void abc() throws FileNotFoundException {
+        try {
+            File f = new File("test10.txt");
+            FileInputStream fis = new FileInputStream(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception poyman i nemnogo obrabotan");
+            throw e;
+        } finally {
+            System.out.println("eto block finaly");
+        }
+    }
+
+    void method() {
+        try {
+            abc();
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception poyman i nemnogo obrabotan");
+        }
+
+    }
+}
