ccaa4d8a736c6996b62738e762fec55931db69c1
VegasJava <seninlsa22@gmail.com>
2022-12-27 15:40:23 +0400

    need continue from 52:00

---
+++ Lesson28/Test3.java
@@ -0,0 +1,16 @@
+package Lesson28;
+
+import java.time.*;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+        LocalDate ld1 = LocalDate.of(2014, 5, 31);
+        LocalDate ld2 = LocalDate.of(2012,Month.JUNE,18);
+        System.out.println(ld1.isAfter(ld2));
+        System.out.println(ld1.isBefore(ld2));
+        
+        
+        
+    }
+}
